7792f7ee81c3e0f3005cff62e530d84700923629
“Saki <sakisandra.c@gmail.com>
2023-08-01 23:11:23 -0600

    Add api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,40 @@
+import { getRandNum, cleanUpData } from "./utils";
+
+const getKanji = async () => {
+  const res = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/`, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+    }
+  });
+
+  if(!res.ok) {
+    console.log('this i sthe error', res.statusText);
+    throw new Error(`${res.statusText} - Please Try Again`);
+  };
+
+  const data = await res.json();
+  return data;
+}
+
+const getSingleKanji = async (type, char) => {
+  const res = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/${type}/${char}`, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+    }
+  })
+
+  if(!res.ok) {
+    console.log(res.statusText);
+    throw new Error(`${res.statusText} - Please Try Again`);
+  };
+
+  const data = await res.json();
+  return data;
+}
+
+
+export { getSingleKanji, getKanji }
