f97433e9fdb12957359ec72710c40c77db472690
“Saki <sakisandra.c@gmail.com>
2023-08-01 23:19:51 -0600

    Add kanji set of 5 to home

---
+++ src/Components/KanjiSet/KanjiSet.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './KanjiSet.css'
+
+const KanjiSet = ({kanjiSet, changeMainKanji}) => {
+
+  const renderKanjiSet = () => {
+    return kanjiSet.map(kanji => {
+      return (
+        <div className='kanji-container' key={kanji._id} onClick={() => {changeMainKanji(kanji)}}>
+          <p className='kanj-set-char'>{kanji.ka_utf}</p>
+          <p className='kanji-text'>Meaning: {kanji.meaning}</p>
+          <p className='kanji-text'>Onyomi Pronounciation: {kanji.onyomi}</p>
+          <p className='kanji-text'>Kunyomi Pronounciation: {kanji.kunyomi}</p>
+        </div>
+      )
+    })
+  }
+
+  return (
+    <section className='kanji-set-container'>
+      <p className='set-header'>Random Kanji Set of the Day</p>
+      <div className='kanji-set-box'>
+        {renderKanjiSet()}
+      </div>
+    </section>
+  )
+}
+
+export default KanjiSet
