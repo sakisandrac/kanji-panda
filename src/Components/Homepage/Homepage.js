fa8048fbad30b2d88c00b36b78ab693de87dba3f
“Saki <sakisandra.c@gmail.com>
2023-08-01 23:35:02 -0600

    Add header to homepage

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -3,18 +3,18 @@ import RandomKanji from '../RandomKanji/RandomKanji';
 import './Homepage.css'
 import KanjiSet from '../KanjiSet/KanjiSet';
 
-const Homepage = ({setKanjiSet, setGetMoreKanji, mainKanji, kanjiSet, changeMainKanji, saveKanji, savedKanji}) => {
+const Homepage = ({setKanjiSet, mainKanji, kanjiSet, changeMainKanji, saveKanji, savedKanji}) => {
   const handleClick = () => {
     setKanjiSet([]);
-    // setGetMoreKanji(prev => !prev);
   }
 
   return (
     <div className='main-container'>
       <main className='dashboard'>
+        <h1 className='header'>Let's Study Kanji!</h1>
         <RandomKanji saveKanji={saveKanji} mainKanji={mainKanji} savedKanji={savedKanji}/>
         <KanjiSet saveKanji={saveKanji}  kanjiSet={kanjiSet} changeMainKanji={changeMainKanji}/>
-        <button className='btn' onClick={handleClick}>Get Another Set!</button>
+        <button className='save-btn' onClick={handleClick}>Get Another Set!</button>
       </main>
     </div>
   )
