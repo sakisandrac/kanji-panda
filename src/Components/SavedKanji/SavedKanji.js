e171abb2a49e6d3469f4ab8f6bc670abd2bde6e2
“Saki <sakisandra.c@gmail.com>
2023-08-01 23:37:14 -0600

    Add header to saved kanji

--- src/Components/SavedKanji/SavedKanji.js
+++ src/Components/SavedKanji/SavedKanji.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import RandomKanji from '../RandomKanji/RandomKanji'
+import React from 'react';
+import RandomKanji from '../RandomKanji/RandomKanji';
+import './SavedKanji.css';
 
 const SavedKanji = ({saveKanji, savedKanji}) => {
   return (
-    <div className='saved-container'>
-      {savedKanji.map(k => {
-        return (
-          <RandomKanji mainKanji={k} saveKanji={saveKanji} savedKanji={savedKanji} />
-        )
-      })}
+    <div className='saved-page'>
+      <main className='dashboard'>
+        <h1 className='header'>My Saved Kanji</h1>
+        <div className='saved-container'>
+          {savedKanji.map(k => {
+            return (
+              <RandomKanji mainKanji={k} saveKanji={saveKanji} savedKanji={savedKanji} />
+            )
+          })}
+        </div>
+      </main>
     </div>
   )
 }
